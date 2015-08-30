e03b01b153ed55ca2ca19fef9ad5200edd45fcd2
sh4869 <nobuk4869@gmail.com>
2015-08-30 23:27:57 +0900

    dartfmt retry

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -3,25 +3,18 @@
 library twitter.client;
 
 import 'dart:async';
-
 import 'dart:convert';
 
 import 'package:http/http.dart' as http;
-
 import 'package:oauth/oauth.dart' as oauth;
 
 class Client extends oauth.Client {
 
   /// Constructs a new Client with [oauth.Tokens]
-
   Client(oauth.Tokens tokens) : super(tokens);
 
   /// Send an HTTP request and Return [http.Response] with the given headers and body to the given
-
   /// URL, which can be a [Uri] or a [String] and [method].
-
-  ///
-
   Future<http.Response> request(String method, url,
       {Map<String, String> headers, body, Encoding encoding}) {
     if (url is String) url = Uri.parse(url);
@@ -29,9 +22,7 @@ class Client extends oauth.Client {
     var request = new http.Request(method, url);
 
     if (headers != null) request.headers.addAll(headers);
-
     if (encoding != null) request.encoding = encoding;
-
     if (body != null) {
       if (body is String) {
         request.body = body;
