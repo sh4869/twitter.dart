54fc4f6acfb691e23bad8ab5a7cfda50e79c2c62
sh4869 <nobuk4869@gmail.com>
2016-02-12 23:37:29 +0900

    add Test for new Constrcutor

--- test/test.dart
+++ test/test.dart
@@ -3,12 +3,27 @@ import 'dart:io';
 import 'package:twitter/twitter.dart';
 import 'package:test/test.dart';
 
-main(){ 
-	test("Simple API TEST",() async {
-		var envVars = Platform.environment;
-		var twitter = new Twitter(envVars['CONSUMER_KEY'],envVars['CONSUMER_SECRET'],
-			envVars['ACCESS_TOKEN'],envVars['ACCESS_TOKEN_SECRET']);	
-		var response = await twitter.request("GET","statuses/user_timeline.json");
-		expect(response.body,isNotNull);
-	});
+main() {
+  test("Simple API TEST", () async {
+    var envVars = Platform.environment;
+    var twitter = new Twitter(
+        envVars['CONSUMER_KEY'],
+        envVars['CONSUMER_SECRET'],
+        envVars['ACCESS_TOKEN'],
+        envVars['ACCESS_TOKEN_SECRET']);
+    var response = await twitter.request("GET", "statuses/user_timeline.json");
+    expect(response.body, isNotNull);
+  });
+  test("Twitter.fromMap Test", () async {
+    var envVars = Platform.environment;
+    Map keyMap = {
+      "consumerKey": envVars['CONSUMER_KEY'],
+      "consumerSecret": envVars['CONSUMER_SECRET'],
+      "accessToken": envVars['ACCESS_TOKEN'],
+      "accessSecret": envVars['ACCESS_TOKEN_SECRET']
+    };
+    var twitter = new Twitter.fromMap(keyMap);
+    var response = await twitter.request("GET", "statuses/user_timeline.json");
+    expect(response.body,isNotNull);
+  });
 }
