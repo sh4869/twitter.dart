eb2ebffac4aa608f4c078aec8da9b89822927921
sh4869 <nobuk4869@gmail.com>
2018-12-30 02:37:46 +0900

    Fix example tweet

--- exapmple/posttweet.dart
+++ exapmple/posttweet.dart
@@ -8,20 +8,27 @@ main() {
   print(envVars['ACCESS_TOKEN']);
   print(envVars['ACCESS_TOKEN_SECRET']);
   var map = {
-      "consumerKey": envVars['CONSUMER_KEY'],
-      "consumerSecret": envVars['CONSUMER_SECRET'],
-      "accessToken": envVars['ACCESS_TOKEN'],
-      "accessSecret": envVars['ACCESS_TOKEN_SECRET']
+    "consumerKey": envVars['CONSUMER_KEY'],
+    "consumerSecret": envVars['CONSUMER_SECRET'],
+    "accessToken": envVars['ACCESS_TOKEN'],
+    "accessSecret": envVars['ACCESS_TOKEN_SECRET']
   };
+
   Twitter twitter = new Twitter.fromMap(map);
-  
+
   try {
-    var a = twitter.request("POST", "statuses/update.json",body: {"status":"test"});
-    a.then((value){
+    var a = twitter.request("POST", "statuses/update.json",
+        body: {"status": "@TOS test 7"});
+    a.then((value) {
       new File("test.json").writeAsString(value.body);
+      twitter.request("POST", "statuses/update.json",
+          body: {"status": "@TOS test 8"});
+    }).then((value) {
+      twitter.close();
     });
-  } catch(e) {
-  } finally {
-  }
+  } catch (e) {
+    print(e);
+  } finally {}
+
   return;
 }
